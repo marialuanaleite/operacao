1c1c652f566cba5e6d2221a09d1390d8533fb774
Maria Luana <marialuanaleite@gmail.com>
2017-01-18 18:21:30 -0200

    Primeiro projeto.

---
+++ src/br/com/operacao/Principal.java
@@ -0,0 +1,10 @@
+package br.com.operacao;
+
+/**
+ * Created by maria on 18/01/17.
+ */
+public class Principal {
+    public static void main(String[]args){
+        System.out.println("Teste");
+    }
+}
